90e40570e29fde2d7945808297dcbaaa8807dbad
andr-lozov <84988069+andr-lozov@users.noreply.github.com>
2026-04-02 22:54:57 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from database import db, init_db
