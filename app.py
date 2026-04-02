6ef7103b427e420e0d97b0f4e9a77fce4344d766
andr-lozov <84988069+andr-lozov@users.noreply.github.com>
2026-04-03 00:08:11 +0300

    Update app.py

--- app.py
+++ app.py
@@ -153,6 +153,6 @@ def get_history():
     messages.reverse()
     return jsonify({'success':True,'messages':[m.to_dict() for m in messages]})
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+#if __name__ == '__main__':
+    #port = int(os.environ.get('PORT', 5000))
+    #app.run(host='0.0.0.0', port=port)
