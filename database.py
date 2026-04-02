9befbc7ead99473a3c586a2efc382546374d2f04
andr-lozov <84988069+andr-lozov@users.noreply.github.com>
2026-04-03 00:34:19 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,11 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
-import os
 
 db = SQLAlchemy()
 
 def init_db(app):
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+    """Инициализирует базу данных с приложением"""
     db.init_app(app)
-    # ⚠️ db.create_all() УБРАН отсюда! Он больше не будет ронять старт.
+    with app.app_context():
+        db.create_all()
